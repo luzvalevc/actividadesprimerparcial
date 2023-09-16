b7b0b4cf23beeb8a155641fdf522dcf8bbb07b54
luzvalevc <145036182+luzvalevc@users.noreply.github.com>
2023-09-15 19:02:49 -0600

    Create repasoprimerparcial2.py

---
+++ repasoprimerparcial2.py
@@ -0,0 +1,9 @@
+def f(x): "temperatura"
+print ("convierte grados celsius a fahrenheit")
+print ("inserte los grados celsius")
+
+celsius=input()
+print("los grados fahrenheit son")
+
+operacion= int(celsius)*1.8+32
+print(operacion)
