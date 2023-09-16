824747765a7bad57eeeb5a068d421bb95e706c4b
luzvalevc <145036182+luzvalevc@users.noreply.github.com>
2023-09-15 19:00:13 -0600

    Create repasoprimerparcial1.py

---
+++ repasoprimerparcial1.py
@@ -0,0 +1,13 @@
+def f(x): "buscando el area de un rectangulo"
+print ("inserte las medidas del rectangulo")
+longitud=input()
+ancho=input()
+print("el area del rectangulo es")
+
+operacion= int(longitud)*int(ancho)
+print(operacion)
+
+print("el perimetro del rectangulo es")
+
+operacion= int(longitud)*2+int(ancho)*2
+print(operacion)
