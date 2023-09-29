14290093fdf59f64af6b3c8871fd916c0befec0d
luzvalevc <145036182+luzvalevc@users.noreply.github.com>
2023-09-28 19:48:53 -0600

    Create cod_olimpiadas1.py

---
+++ cod_olimpiadas1.py
@@ -0,0 +1,28 @@
+def f(x):"suma y resta de dos numeros "
+print("ingrese los dos numeros")
+numero1= input()
+numero2= input()
+print("la suma de estos dos numeros es")
+operacion= int(numero1)+int(numero2)
+print(operacion)
+print("la resta de estos dos numeros es")
+operacion=int(numero1)-int(numero2)
+print(operacion)
+
+def f(x):"multiplicacion de dos numeros"
+print("dame los dos numeros")
+numero1= input()
+numero2= input()
+print("la division de estos numeros es")
+operacion= int(numero1)/int(numero2)
+print(operacion)
+print("la multiplicacion de estos numeros es")
+operacion= int(numero1)*int(numero2)
+print(operacion)
+
+def f(x):"conversion de metros a kilometros"
+print("ingrese la cantidad de kilometros")
+kilometros= input()
+print("la cantidad de metros es")
+operacion= int(kilometros)*1000
+print(operacion)
